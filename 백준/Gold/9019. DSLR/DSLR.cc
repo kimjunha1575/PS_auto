fbc8a2c4d9e1cf5ddbd47c4401d28fbd76ae4163
Junha Kim <kimjunha1575@gmail.com>
2023-09-05 21:23:05 +0900

    [Gold IV] Title: DSLR, Time: 1620 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.cc
+++ 백준/Gold/9019. DSLR/DSLR.cc
@@ -2,22 +2,11 @@
 #include <queue>
 using namespace std;
 
-struct INT {
-    int value;
-    string path;
-};
-
-// 어차피 한번 들른 수는 다시 안가
-// visited에 어디서 왔는지, 무슨 연산으로 왔는지 기록하면?
-// target > origin 순서로 넘어가면서 기록 복기 가능
-// 해보자
-
 struct History {
     int value;
     char command;
 };
 
-bool visited[10000];
 History visited2[10000];
 
 int D(int n) {
@@ -37,7 +26,7 @@ int R(int n) {
     return (n / 10) + (n % 10) * 1000;
 }
 
-int BFS2(int ori, int target) {
+void BFS2(int ori, int target) {
     for (int i = 0; i < 10000; i++)
         visited2[i] = {-1, 0};
     queue<int> que;
@@ -49,7 +38,7 @@ int BFS2(int ori, int target) {
         que.pop();
 
         if (now == target) {
-            return now;
+            return;
         }
 
         // DSLR
@@ -77,36 +66,6 @@ int BFS2(int ori, int target) {
             que.push(tmp);
         }
     }
-    return -1;
-}
-
-string BFS(int ori, int target) {
-    queue<INT> que;
-    que.push({ori, ""});
-
-    while (not que.empty()) {
-        INT now = que.front();
-        que.pop();
-        visited[now.value] = true;
-
-        if (now.value == target) {
-            return now.path;
-        }
-
-        // DSLR
-        int tmp = D(now.value);
-        if (not visited[tmp]) que.push({tmp, now.path + "D"});
-
-        tmp = S(now.value);
-        if (not visited[tmp]) que.push({tmp, now.path + "S"});
-
-        tmp = L(now.value);
-        if (not visited[tmp]) que.push({tmp, now.path + "L"});
-
-        tmp = R(now.value);
-        if (not visited[tmp]) que.push({tmp, now.path + "R"});
-    }
-    return "failed";
 }
 
 int main(void) {
@@ -115,24 +74,17 @@ int main(void) {
     for (int test = 1; test <= T; test++) {
         int ori, target;
         cin >> ori >> target;
-        int ans2 = BFS2(ori, target);
-        // cout << ans2 << "\n";
+        BFS2(ori, target);
         string ans = "";
-        while (ans2 != ori) {
+        while (target != ori) {
             string tmp = "0";
-            tmp[0] = visited2[ans2].command;
-            // cout << tmp;
+            tmp[0] = visited2[target].command;
             ans += tmp;
-            ans2 = visited2[ans2].value;
+            target = visited2[target].value;
         }
-        // cout << ans;
         for (int i = 0; i < ans.length(); i++) {
-            cout << ans[ans.length()-1-i];
+            cout << ans[ans.length() - 1 - i];
         }
         cout << "\n";
-        // string ans = BFS(ori, target);
-        // cout << "#" << test << " " << ans << "\n";
-        // for (int i = 0; i < 10000; i++)
-            // visited[i] = false;
     }
 }
