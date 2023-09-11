67e71af376a285fd1f9c8b69ebe3047699be0b56
Junha Kim <kimjunha1575@gmail.com>
2023-09-11 17:56:20 +0900

    [Gold III] Title: 개구리 점프, Time: 0 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.cc
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.cc
@@ -2,79 +2,107 @@
 #include <queue>
 using namespace std;
 
+// 문제 링크
+// https://www.acmicpc.net/problem/17619
+
+// 통나무 N개 (가로 방향)
+// 한 통나무에서 다른 통나무로 수직방향으로만 점프 가능
+// 다른 통나무 위를 지날 수 없다 << 이걸 모르겠네
+
+// 통나무 개수 N
+// 질문의 개수 Q
+// N개의 줄에 x1, x2, y
+// Q개의 줄에 start, dest
+
+// 통나무 개수 <= 100,000
+// 질문 개수 <= 100,000
+
 struct Log {
-    int start;
-    int end;
-    int y;
-    int id;
+    int num;
+    int x1, x2, y;
 
-    // Log(int start, int end, int y, int id) : start(start), end(end), y(y), id(id) {
-    // }
-
-    bool operator<(Log l) const {
-        if (start < l.start)
+    bool operator<( Log l ) const {
+        // ascending ordering with x1 (start point)
+        if (x1 < l.x1) {
             return false;
-        else if (start == l.start && y <= l.y)
-            return false;
+        }
+        else if (x1 == l.x1) {
+            if (x2 < l.x2) {
+                return false;
+            }
+            else if (x2 == l.x2) {
+                return y < l.y;
+            }
+        }
         return true;
     }
 };
 
 int parent[100001];
-Log logs[100001];
-priority_queue<Log> pq;
-int N, Q;
-
-void input() {
-    cin >> N >> Q;
-    for (int i = 1; i <= N; i++) {
-        int start, end, y;
-        cin >> start >> end >> y;
-        pq.push({start, end, y, i});
-        parent[i] = i;
-        logs[i].start = start;
-        logs[i].end = end;
-        logs[i].y = y;
-        logs[i].id = i; 
-    }
-}
 
 int Find(int n) {
-    if (parent[n] == n)
+    if (parent[n] == n) {
         return n;
-    else
+    }
+    else {
         return parent[n] = Find(parent[n]);
+    }
 }
 
 void Union(int n1, int n2) {
-    int r1 = Find(n1);
-    int r2 = Find(n2);
-    parent[r2] = r1;
+    int p1 = Find(n1);
+    int p2 = Find(n2);
+
+    if (p1 != p2) {
+        parent[p1] = p2;
+    }
 }
 
+int N, Q;
+priority_queue<Log> logs;
+
 int main(void) {
-    input();
-    while (pq.size() > 1) {
-        Log cur = pq.top();
-        pq.pop();
+    cin >> N >> Q;
+    int num = 1;
+    for (int i = 0; i < N; i++) {
+        int x1, x2, y;
+        cin >> x1 >> x2 >> y;
+        parent[num] = num;
+        logs.push({num++, x1, x2, y});
+        
+    }
 
-        Log next = pq.top();
+    // Union Find task
+    while (not logs.empty()) {
+        Log now = logs.top();
+        logs.pop();
 
-        if (logs[Find(cur.id)].end >= next.start /*&& cur.y != next.y*/) {
-            if (logs[Find(cur.id)].end > next.end)
-                Union(cur.id, next.id);
-            else
-                Union(next.id, cur.id);
-            // printf("can move to %d to %d\n", cur.id, next.id);
-            // printf("%d.end: %d\n%d.start: %d\n", cur.id, cur.end, next.id, next.start);
+        int num = now.num;
+        int x1 = now.x1;
+        int x2 = now.x2;
+        int y = now.y;
+
+        Log next = logs.top();
+
+        if (next.x1 <= x2 && next.y != y) {
+            Union(num, next.num);
+            // printf("Union %d, %d\n", num, next.num);
         }
     }
+
+
     for (int i = 0; i < Q; i++) {
-        int n1, n2;
-        cin >> n1 >> n2;
-        if (Find(n1) == Find(n2))
-            printf("1\n");
-        else
-            printf("0\n");
+        int start, dest;
+        cin >> start >> dest;
+        // print result
+
+        if (Find(start) == Find(dest)) {
+            cout << 1 << endl;
+        }
+        else {
+            cout << 0 << endl;
+        }
     }
+
+    return 0;
 }
