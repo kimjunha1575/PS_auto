676238a6ac5cb376a042970b84941568901f05bd
Junha Kim <kimjunha1575@gmail.com>
2023-09-15 14:40:21 +0900

    [Gold V] Title: 동전 2, Time: 100 ms, Memory: 2988 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// https://www.acmicpc.net/problem/2294
+
+// 동전의 가지수
+int cntCoin;
+// 만들어야 할 가치
+int target;
+// 동전의 종류
+int coins[101];
+// dp[n]: n원을 최소 몇 개의 동전으로 만들 수 있는지
+int dp[200000];
+
+void input() {
+    cin >> cntCoin >> target;
+    for (int i = 1; i <= cntCoin; i++) {
+        cin >> coins[i];
+    }
+}
+
+void solve(int cnt, int worth) {
+    if (worth > target) return;
+    dp[worth] = cnt;
+    // printf("dp[%d] = %d\n", worth, cnt);
+    for (int i = 1; i <= cntCoin; i++) {
+        int next = worth + coins[i];
+        if (dp[next] == 0 || (dp[next] && dp[next] > cnt + 1)) solve(cnt + 1, next);
+    }
+}
+
+int main(void) {
+    input();
+    solve(0, 0);
+    int ans = dp[target];
+    if (ans)
+        printf("%d", ans);
+    else
+        printf("-1");
+}
