597af42efa9acecfd772182e92090febec60e25d
Junha Kim <kimjunha1575@gmail.com>
2023-09-18 17:17:43 +0900

    [Gold IV] Title: 도둑, Time: 332 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/13422. 도둑/도둑.cc
+++ 백준/Gold/13422. 도둑/도둑.cc
@@ -1,28 +1,18 @@
 #include <iostream>
 using namespace std;
 
+// https://www.acmicpc.net/problem/13422
+
 #define MAX_HOUSE_NUM 100000
 #define MAX_ALERT_LIMIT 1000000000
 
-// N개의 집이 원형으로 이웃한 마을
-// 각자 자신의 집에 돈을 보관
-// 각 집의 숫자는 돈의 금액을 나타낸다
-// 도둑은 M개의 연속된 집에서, 각각의 집에 보관중인 모든 돈을 훔친다
-// 도둑이 K원 이상의 돈을 훔치면 도둑은 바로 붙잡힌다
-
-// input
-// 테스트 케이스의 수 T
-// 각 테스트케이스 마다
 // 집의 개수 N
 // 도둑이 돈을 훔칠 집의 개수 M
 // 방범장치가 작동하는 최소 돈의 양 K
-// 각 집에서 보관중인 돈의 양이 순서대로 주어진다
-
-// output
-// 도둑이 붙잡히지 않고 돈을 훔칠 수 있는 방법의 수를 출력한다
-
 int N, M, K;
+// 도둑이 현재 훔친 돈의 총량의 초기값
 int total;
+// 각 집의 정보를 저장할 배열(circular 형태이므로 크기값 2배로 할당)
 int map[2*MAX_HOUSE_NUM + 1];
 
 
@@ -31,29 +21,22 @@ void input() {
     for (int i = 0; i < N; i++) {
         cin >> map[i];
         map[N+i] = map[i];
+        // input 받으면서 total 초기값 계산
         total += map[i];
     }
 }
 
-// N개의 집에 대해 한 방향으로 M개의 집을 털 수 있나 검사?
-// 시간초과 예상됨
-
-// 투포인터 이용해서 가야할듯. 배열 크기 두배로 하거나 나머지 연산자 이용해서.
-// 슬라이딩 윈도우?
-// 가지수를 찾는 문제라서 이분탐색은 적용하기 힘들어보임
-
-// sliding window면 O(N)으로 처리 가능할 듯 함
-// 배열 크기는 널널하니까 두배 해서 잡으면 될 듯
-
 int solve() {
     int res = 0;
     // sliding window의 초기값 구성
+    // 0번 집부터 M-1번 집까지 M개의 집을 터는 경우, 그 경우 훔치는 돈
     int left = 0;
     int right = M-1;
     int cur = 0;
     for (int i = left; i <= right; i++) cur += map[i];
 
     // 탐색 시작
+    // n개의 경우에 대해 한 칸씩 이동하면서 조건에 맞는 경우 res++
     for (int i = 0; i < N; i++) {
         if (cur < K) res++;
         cur -= map[left++];
@@ -66,12 +49,16 @@ int main(void) {
     int T;
     cin >> T;
     for (int test = 1; test <= T; test++) {
+        // 도둑이 훔친 돈 초기화
         total = 0;
         input();
+        // 모든 집에서 돈을 훔쳐야 하고, 그것이 가능한 경우,
+        // 가능한 가짓수는 모든 집에서 돈을 훔치는 단 한 가지이기 때문에 1 출력
         if (N == M and total < K) {
             cout << 1 << "\n";
             continue;
         }
+        // 다른 경우 가능한 총 경우의 수를 계산 후 출력
         int ans = solve();
         cout << ans << "\n";
     }
