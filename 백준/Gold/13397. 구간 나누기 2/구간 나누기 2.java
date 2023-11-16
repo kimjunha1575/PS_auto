ad6737b4a937141ba32c64dc16574b9875cb4611
Junha Kim <kimjunha1575@gmail.com>
2023-11-16 16:13:13 +0900

    [Gold IV] Title: 구간 나누기 2, Time: 152 ms, Memory: 15048 KB -BaekjoonHub

--- 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
+++ 백준/Gold/13397. 구간 나누기 2/구간 나누기 2.java
@@ -2,41 +2,38 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// https://www.acmicpc.net/problem/13397
+
 public class Main {
 
 	static int[] arr;
-	static int arr_size;
-	static int group_size;
+	static int arrSize;
+	static int groupSize;
 
 	public static void main(String[] args) throws IOException {
 		getInput();
-		int ans = binarySearch();
-		System.out.println(ans);
+		System.out.println(binarySearch());
 	}
 
 	public static boolean isPossible(int n) {
-		// System.out.printf("=======n: %d=======\n", n);
 		int min = Integer.MAX_VALUE;
-		int max = 0;
-		int group_cnt = 1;
-		for (int i = 0; i < arr_size; i++) {
-			int min_tmp = min;
-			int max_tmp = max;
-			if (arr[i] < min_tmp) min_tmp = arr[i];
-			if (arr[i] > max_tmp) max_tmp = arr[i];
-			// System.out.printf("i: %d\nmin: %d\nmax: %d\n", i, min_tmp, max_tmp);
-			if (max_tmp - min_tmp <= n) {
-				min = min_tmp;
-				max = max_tmp;
+		int max = Integer.MIN_VALUE;
+		int cntGroup = 1;
+		for (int i = 0; i < arrSize; i++) {
+			int tmpMin = min;
+			int tmpMax = max;
+			if (arr[i] < tmpMin) tmpMin = arr[i];
+			if (arr[i] > tmpMax) tmpMax = arr[i];
+			if (tmpMax - tmpMin <= n) {
+				min = tmpMin;
+				max = tmpMax;
 			} else {
-				// System.out.println("new group");
-				group_cnt++;
+				cntGroup++;
 				min = arr[i];
 				max = arr[i];
-				if (group_cnt > group_size) return false;
+				if (cntGroup > groupSize) return false;
 			}
 		}
-		// System.out.println("true");
 		return true;
 	}
 
@@ -48,7 +45,6 @@ public class Main {
 			int mid = (left + right) / 2;
 			if (isPossible(mid)) right = mid - 1; else left = mid + 1;
 		}
-		// System.out.printf("left: %d\nright: %d\n", left, right);
 		if (left >= 0) return left; else return right;
 	}
 
@@ -56,11 +52,11 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input;
 		input = br.readLine().split(" ");
-		arr_size = Integer.parseInt(input[0]);
-		arr = new int[arr_size];
-		group_size = Integer.parseInt(input[1]);
+		arrSize = Integer.parseInt(input[0]);
+		arr = new int[arrSize];
+		groupSize = Integer.parseInt(input[1]);
 		input = br.readLine().split(" ");
-		for (int i = 0; i < arr_size; i++) {
+		for (int i = 0; i < arrSize; i++) {
 			arr[i] = Integer.parseInt(input[i]);
 		}
 		br.close();
