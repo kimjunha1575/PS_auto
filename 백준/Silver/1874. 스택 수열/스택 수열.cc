ad71ee471d63568be9cb62c3efee0702c290a009
Junha Kim <kimjunha1575@gmail.com>
2024-06-14 08:33:15 +0900

    [Silver II] Title: 스택 수열, Time: 44 ms, Memory: 2784 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int N;
+stack<int> st;
+
+int main(void) {
+    string ans = "+\n";
+    int cur = 1;
+    st.push(cur++);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int tmp;
+        cin >> tmp;
+        if (st.empty() || tmp > st.top()) {
+            st.push(cur++);
+            ans += "+\n";
+            while (tmp > st.top()) {
+                st.push(cur++);
+                ans += "+\n";
+            }
+            ans += "-\n";
+            st.pop();
+        } else if (tmp == st.top()) {
+            ans += "-\n";
+            st.pop();
+        } else if (tmp < st.top()) {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << ans;
+    return 0;
+}
