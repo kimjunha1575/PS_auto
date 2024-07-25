96b059de7f9598cbf43324644b1f44ee155fb795
Junha Kim <kimjunha1575@gmail.com>
2024-07-25 19:45:15 +0900

    [Silver I] Title: 오목, Time: 108 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,10 +1,4 @@
-board = []
-for _ in range(19):
-    row = list(map(int, input().split()))
-    board.append(row)
-
-
-def who_is_winner(r, c):
+def is_game_over(r, c):
     global win_y
     global win_x
     color = board[r][c]
@@ -15,13 +9,17 @@ def who_is_winner(r, c):
         pc = c - dc[di]
         if 0 <= pr < 19 and 0 <= pc < 19 and board[pr][pc] == color:
             continue
+        nr = r
+        nc = c
         length = 1
-        nr = r + dr[di]
-        nc = c + dc[di]
-        while 0 <= nr < 19 and 0 <= nc < 19 and board[nr][nc] == color:
-            length += 1
+        for _ in range(5):
             nr += dr[di]
             nc += dc[di]
+            if 0 <= nr < 19 and 0 <= nc < 19:
+                if board[nr][nc] == color:
+                    length += 1
+                else:
+                    break
         if length == 5:
             win_y = r + 1
             win_x = c + 1
@@ -29,21 +27,24 @@ def who_is_winner(r, c):
     return 0
 
 
-def search_winner():
+def search():
     for y in range(19):
         for x in range(19):
             if board[y][x] > 0:
-                winner = who_is_winner(y, x)
+                winner = is_game_over(y, x)
                 if winner:
                     return winner
 
 
+board = []
+for _ in range(19):
+    row = list(map(int, input().split()))
+    board.append(row)
 win_y = 0
 win_x = 0
-ans = search_winner()
+ans = search()
 if ans:
     print(ans)
     print(win_y, win_x)
 else:
     print(0)
-
