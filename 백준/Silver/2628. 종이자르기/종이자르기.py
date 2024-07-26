77c3741ee648ad07b54e52418e72033a2ed91549
Junha Kim <kimjunha1575@gmail.com>
2024-07-26 10:36:50 +0900

    [Silver V] Title: 종이자르기, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -11,17 +11,15 @@ for _ in range(cut):
 
 hor.sort()
 ver.sort()
+max_width = width
+max_height = height
 if len(ver):
     max_width = max(ver[0], width - ver[-1])
-else:
-    max_width = width
+    for p in range(1, len(ver)):
+        max_width = max(max_width, ver[p] - ver[p-1])
 if len(hor):
     max_height = max(hor[0], height - hor[-1])
-else:
-    max_height = height
-for p in range(1, len(ver)):
-    max_width = max(max_width, ver[p] - ver[p-1])
-for p in range(1, len(hor)):
-    max_height = max(max_height, hor[p] - hor[p-1])
+    for p in range(1, len(hor)):
+        max_height = max(max_height, hor[p] - hor[p-1])
 
 print(max_width * max_height)
