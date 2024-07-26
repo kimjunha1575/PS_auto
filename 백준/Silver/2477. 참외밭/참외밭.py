0841409510eef76636bc7fc51dbc7c6d1605694a
Junha Kim <kimjunha1575@gmail.com>
2024-07-26 20:27:21 +0900

    [Silver II] Title: 참외밭, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -4,7 +4,7 @@ order = [None] * 4
 case = [0] * 6
 for i in range(6):
     di, dist = map(int, input().split())
-    move.append((di, dist))
+    move.append(dist)
     if order[di-1] is None:
         order[di-1] = i
     else:
@@ -13,25 +13,15 @@ for i in range(6):
 total = 1
 for i in range(len(case)):
     if case[i] == 0:
-        total *= move[i][1]
+        total *= move[i]
 case = ''.join(map(str, case))
-if "1111" in case:
-    i = case.find("1111")
-    sub = move[i+1][1] * move[i+2][1]
+if case == "111001":
+    total -= move[0] * move[1]
 elif case == "110011":
-    sub = move[0][1] * move[5][1]
+    total -= move[0] * move[5]
 elif case == "100111":
-    sub = move[4][1] * move[5][1]
-elif case == "111001":
-    sub = move[0][1] * move[1][1]
-
-print((total - sub) * K)
-
-'''
-        x x x _ _ x -> 첫 두개
-        x _ _ x x x -> 마지막 두개
-        x x _ _ x x -> 처음과 마지막
-        x x x x     -> 2,3번째
-    - 안들어간 곳 3개 (변형 없는 곳)
-        - 어떻게 돌든 4번 중 2,3번째가 들어간 곳
-'''
+    total -= move[4] * move[5]
+else:
+    i = case.find("1111")
+    total -= move[i+1] * move[i+2]
+print(total * K)
