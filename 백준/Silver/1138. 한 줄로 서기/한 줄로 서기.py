ec46ec6a5c9871b3ee36357a5296fb4ae50352d2
Junha Kim <kimjunha1575@gmail.com>
2024-07-29 10:14:47 +0900

    [Silver II] Title: 한 줄로 서기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -0,0 +1,13 @@
+N = int(input())
+tallers = list(map(int, input().split()))
+ans = [0] * N
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if ans[j] == 0:
+            if cnt == tallers[i]:
+                ans[j] = i + 1
+                break
+            cnt += 1
+
+print(*ans, sep=' ')
