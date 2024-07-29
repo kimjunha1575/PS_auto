0c4544aa59804ba0f542e64ae504312b3becc701
Junha Kim <kimjunha1575@gmail.com>
2024-07-29 19:33:49 +0900

    [Silver IV] Title: 빙고, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,16 +1,22 @@
 def check_board(i, j):
     global board
     global bingo
+    # 빙고가 이미 완성된 칸은 더이상 불리지 않으므로
+    # 새롭게 불린 숫자의 칸의 행, 열, 대각선만 검사 후 누적
+    
+    # 해당 칸의 열 검사
     for col in range(5):
         if board[i][col] > 0:
             break
         if col == 4:
             bingo += 1
+    # 해당 칸의 행 검사
     for row in range(5):
         if board[row][j] > 0:
             break
         if row == 4:
             bingo += 1
+    # 해당 칸이 대각선 빙고가 가능하다면 해당 대각선 검사
     if i == j:
         for rc in range(5):
             if board[rc][rc] > 0:
@@ -23,6 +29,8 @@ def check_board(i, j):
                 break
             if rc == 4:
                 bingo += 1
+
+    # 누적된 빙고가 3개 이상이라면 게임 끝
     if bingo >= 3:
         return True
     return False
@@ -32,6 +40,7 @@ def delete_number(num):
     global board
     for i in range(5):
         for j in range(5):
+            # 불린 숫자는 -1로 표기
             if board[i][j] == num:
                 board[i][j] = -1
                 return i, j
@@ -46,9 +55,11 @@ for _ in range(5):
     nums += list(map(int, input().split()))
 cnt = 0
 for num in nums:
+    # 부른 숫자 카운트
     cnt += 1
+    # 불린 숫자를 칠하고
     i, j = delete_number(num)
-    done = check_board(i, j)
-    if done:
+    # 빙고가 3개 이상이 되었다면 게임 끝
+    if check_board(i, j):
         break
 print(cnt)
