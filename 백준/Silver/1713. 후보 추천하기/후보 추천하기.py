e8755315927d1c6b01b616468a4dd4ff32323866
Junha Kim <kimjunha1575@gmail.com>
2024-07-29 11:31:26 +0900

    [Silver I] Title: 후보 추천하기, Time: 124 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -1,45 +1,40 @@
 N = int(input())
 M = int(input())
-thumbs = list(map(int, input().split()))
-cands = [None] + [[0, 0] for _ in range(100)]
+votes = list(map(int, input().split()))
+candidates = [None] + [[0, 0] for _ in range(100)]
 pictures = [None for _ in range(N)]
 
 cnt = 0
-for i in range(len(thumbs)):
-    thumb = thumbs[i]
-    # 기존 후보
-    if cands[thumb][0]:
-        cands[thumb][0] += 1
-    # 새로운 후보, 빈자리 없음
+for i in range(len(votes)):
+    thumb = votes[i]
+    if candidates[thumb][0]:
+        candidates[thumb][0] += 1
     elif cnt == N:
-        lowest = 1001
-        lowest_idx = 1001
+        lowest_vote = M+1
+        lowest_idx = M+1
+        lowest_cand = 0
         for j in range(N):
-            cur = cands[pictures[j][0]][0]
+            cur = candidates[pictures[j][0]][0]
             cur_idx = pictures[j][1]
-            if cur < lowest or (cur == lowest and cur_idx < lowest_idx):
-                lowest = min(lowest, cur)
+            if cur < lowest_vote or (cur == lowest_vote and cur_idx < lowest_idx):
+                lowest_vote = cur
                 lowest_idx = pictures[j][1]
                 lowest_cand = pictures[j][0]
-        cands[thumb][0] += 1
-        cands[thumb][1] = cands[lowest_cand][1]
-        pictures[cands[lowest_cand][1]] = [thumb, i]
-        cands[lowest_cand][0] = 0
-        cands[lowest_cand][1] = 0
-    # 새로운 후보, 빈자리 있음
+        candidates[thumb][0] += 1
+        candidates[thumb][1] = candidates[lowest_cand][1]
+        pictures[candidates[lowest_cand][1]] = [thumb, i]
+        candidates[lowest_cand][0] = 0
+        candidates[lowest_cand][1] = 0
     else:
         cnt += 1
         for j in range(N):
             if pictures[j] is None:
                 pictures[j] = [thumb, i]
-                cands[thumb][0] += 1
-                cands[thumb][1] = j
+                candidates[thumb][0] += 1
+                candidates[thumb][1] = j
                 break
 
-for i in range(N-1, -1, -1):
-    if pictures[i] is None:
-        del pictures[i]
-pictures.sort()
+pictures.sort(key=lambda x: x or [M+1, M+1])
 for cand in pictures:
     if cand is not None:
         print(cand[0], end=' ')
