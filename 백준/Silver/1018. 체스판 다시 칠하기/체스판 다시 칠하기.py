4d25de36e4e19e4084cdb0dd0d147a888b1c9a35
Junha Kim <kimjunha1575@gmail.com>
2024-07-30 20:08:39 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 136 ms, Memory: 110600 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,7 +1,3 @@
-white = list("WBWBWBWB")
-black = list("BWBWBWBW")
-white_first = [white, black] * 4
-black_first = [black, white] * 4
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
 ans = 64
@@ -9,11 +5,17 @@ for r in range(N-7):
     for c in range(M-7):
         cnt_white = 0
         cnt_black = 0
-        for i in range(r, r+8):
-            for j in range(c, c+8):
-                if board[i][j] != white_first[r-i][c-j]:
-                    cnt_white += 1
-                if board[i][j] != black_first[r-i][c-j]:
-                    cnt_black += 1
+        for i in range(8):
+            for j in range(8):
+                if (i+j) % 2 == 0:
+                    if board[r+i][c+j] == 'W':
+                        cnt_black += 1
+                    else:
+                        cnt_white += 1
+                else:
+                    if board[r+i][c+j] == 'B':
+                        cnt_black += 1
+                    else:
+                        cnt_white += 1
         ans = min(ans, cnt_white, cnt_black)
 print(ans)
