3db97caf2e44b92c29afb435ad93660b900788ff
Junha Kim <kimjunha1575@gmail.com>
2024-07-30 20:43:46 +0900

    [Gold V] Title: 가로등, Time: 640 ms, Memory: 273284 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/32069. 가로등/가로등.py
+++ 백준/Gold/32069. 가로등/가로등.py
@@ -1,26 +1,23 @@
 from collections import deque
-
-
 L, N, K = map(int, input().split())
 arr = list(map(int, input().split()))
-visited = [0] * (L+1)
 que = deque()
+visited = set()
 for pos in arr:
-    visited[pos] = 1
-    que.append(pos)
+    que.append((pos, 0))
+    visited.add(pos)
 cnt = 0
 while que:
     cnt += 1
-    if cnt >= K:
+    if cnt > K:
         break
     cur = que.popleft()
-    brightness = visited[cur]
-    if L >= cur - 1 >= 0 == visited[cur - 1]:
-        que.append(cur - 1)
-        visited[cur - 1] = brightness + 1
-    if L >= cur + 1 >= 0 == visited[cur + 1]:
-        que.append(cur + 1)
-        visited[cur + 1] = brightness + 1
-visited.sort(key=lambda x: x or L+1)
-for i in range(K):
-    print(visited[i]-1)
+    cur_pos = cur[0]
+    cur_brightness = cur[1]
+    print(cur_brightness)
+    if 0 <= cur_pos - 1 <= L and (cur_pos-1) not in visited:
+        que.append((cur_pos - 1, cur_brightness + 1))
+        visited.add(cur_pos - 1)
+    if 0 <= cur_pos + 1 <= L and (cur_pos+1) not in visited:
+        que.append((cur_pos + 1, cur_brightness + 1))
+        visited.add(cur_pos + 1)
