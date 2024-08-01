319a63e3136a9ef819967a77e4b3bb2f9d690db1
Junha Kim <kimjunha1575@gmail.com>
2024-08-01 13:27:02 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -5,10 +5,8 @@ using namespace std;
 int board_size;
 int region_normal;
 int region_colorblind;
-int visited_normal[100][100];
-int visited_colorblind[100][100];
-char board_normal[100][100];
-char board_colorblind[100][100];
+int visited[100][100];
+int board[100][100];
 int dr[] = {0, 1, 0, -1};
 int dc[] = {1, 0, -1, 0};
 
@@ -18,54 +16,40 @@ void init() {
         for (int c = 0; c < board_size; c++) {
             char tmp;
             cin >> tmp;
-            board_normal[r][c] = tmp;
-            if (tmp == 'R' || tmp == 'G') {
-                board_colorblind[r][c] = 'R';
+            if (tmp == 'R') board[r][c] = 2;
+            else if (tmp == 'G') board[r][c] = 1;
+            else if (tmp == 'B') board[r][c] = 0;
+        }
+    }
+}
+
+void clean() {
+    for (int r = 0; r < board_size; r++) {
+        for (int c = 0; c < board_size; c++) {
+            visited[r][c] = 0;
+        }
+    }
+}
+
+int bfs(int r, int c, int mode) {
+    stack<pair<int, int>> stk;
+    stk.push(make_pair(r, c));
+    visited[r][c] = 1;
+    while (!stk.empty()) {
+        int cr = stk.top().first;
+        int cc = stk.top().second;
+        stk.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nr = cr + dr[dir];
+            int nc = cc + dc[dir];
+            if (nr < 0 || nc < 0 || nr >= board_size || nc >= board_size) continue;
+            if (visited[nr][nc]) continue;
+            if (mode) {
+                if (board[nr][nc] != board[r][c]) continue;
             } else {
-                board_colorblind[r][c] = 'G';
+                if ((board[nr][nc] > 0) != (board[r][c] > 0)) continue;
             }
-        }
-    }
-}
-
-int bfs_normal(int r, int c) {
-    stack<pair<int, int>> stk;
-    stk.push(make_pair(r, c));
-    visited_normal[r][c] = 1;
-    char color = board_normal[r][c];
-    while (!stk.empty()) {
-        int cr = stk.top().first;
-        int cc = stk.top().second;
-        stk.pop();
-        for (int dir = 0; dir < 4; dir++) {
-            int nr = cr + dr[dir];
-            int nc = cc + dc[dir];
-            if (nr < 0 || nc < 0 || nr >= board_size || nc >= board_size) continue;
-            if (visited_normal[nr][nc]) continue;
-            if (board_normal[nr][nc] != color) continue;
-            visited_normal[nr][nc] = 1;
-            stk.push(make_pair(nr, nc));
-        }
-    }
-    return 1;
-}
-
-int bfs_colorblind(int r, int c) {
-    stack<pair<int, int>> stk;
-    stk.push(make_pair(r, c));
-    visited_colorblind[r][c] = 1;
-    char color = board_colorblind[r][c];
-    while (!stk.empty()) {
-        int cr = stk.top().first;
-        int cc = stk.top().second;
-        stk.pop();
-        for (int dir = 0; dir < 4; dir++) {
-            int nr = cr + dr[dir];
-            int nc = cc + dc[dir];
-            if (nr < 0 || nc < 0 || nr >= board_size || nc >= board_size) continue;
-            if (visited_colorblind[nr][nc]) continue;
-            if (board_colorblind[nr][nc] != color) continue;
-            visited_colorblind[nr][nc] = 1;
+            visited[nr][nc] = 1;
             stk.push(make_pair(nr, nc));
         }
     }
@@ -76,14 +60,15 @@ int main(void) {
     init();
     for (int r = 0; r < board_size; r++) {
         for (int c = 0; c < board_size; c++) {
-            if (visited_normal[r][c]) continue;
-            region_normal += bfs_normal(r, c);
+            if (visited[r][c]) continue;
+            region_normal += bfs(r, c, 1);
         }
     }
+    clean();
     for (int r = 0; r < board_size; r++) {
         for (int c = 0; c < board_size; c++) {
-            if (visited_colorblind[r][c]) continue;
-            region_colorblind += bfs_colorblind(r, c);
+            if (visited[r][c]) continue;
+            region_colorblind += bfs(r, c, 0);
         }
     }
     printf("%d %d\n", region_normal, region_colorblind);
