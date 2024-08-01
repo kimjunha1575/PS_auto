d7c950551a3d706976725880b66e7183f4c50f03
Junha Kim <kimjunha1575@gmail.com>
2024-08-01 10:44:38 +0900

    [Silver II] Title: A → B, Time: 120 ms, Memory: 117796 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,16 +1,15 @@
-from collections import deque
 A, B = map(int, input().split())
-q = deque()
-q.append((A, 1))
+q = [(A, 1)]
 a = 0
-while q:
-    v = q[0][0]
-    r = q.popleft()[1]
+h = 0
+while h < len(q):
+    v, r = q[h]
+    h += 1
     if v > B: continue
     if v == B:
         a = r
         break
     q.append((v*2, r+1))
-    q.append((v*10 + 1, r+1))
+    q.append((v*10+1, r+1))
 if a: print(a)
 else: print(-1)
