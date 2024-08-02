700c47947a11549a31ad172a9daf3be73188a3e3
Junha Kim <kimjunha1575@gmail.com>
2024-08-02 19:35:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 288 ms, Memory: 139836 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,31 +1,36 @@
+import sys
+
+
 def get_area(position):
     # 좌표 정보 -> 넓이
     y1, x1, y2, x2 = position
     return areas[y2][x2] - areas[y1-1][x2] - areas[y2][x1-1] + areas[y1-1][x1-1]
 
 
-def render_areas():
-    # 부분합 2차배열 생성
+# 누적합 배열 생성
+def render_prefix():
     global areas
     acc = board[0][0]
-    for r in range(N+1):
+    for r in range(1, N+1):
         acc += board[r][0]
         tmp = acc
-        for c in range(N+1):
-            tmp += areas[r-1][c] + board[r][c]
-            tmp -= areas[r-1][c-1]
+        for c in range(1, N+1):
+            tmp += areas[r-1][c] - areas[r-1][c-1] + board[r][c]
             areas[r][c] = tmp
 
 
 # 입력
-N, M = map(int, input().split())
-board = [[0] * (N+1)] # 좌표 계산을 쉽게 하기 위해 0으로 채운 행과 열 추가함
+N, M = map(int, sys.stdin.readline().split())
+# 좌표 계산을 쉽게 하기 위해 0으로 채운 행과 열 추가함
+board = [[0] * (N+1)]
 for _ in range(N):
-    board.append([0] + list(map(int, input().split())))
-targets = [list(map(int, input().split())) for _ in range(M)]
-areas = [[0] * (N+1) for _ in range(N+1)] # 부분합을 저장할 행렬
-render_areas()
+    board.append([0] + list(map(int, sys.stdin.readline().split())))
+targets = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
 
+# 누적합 배열 생성
+areas = [[0] * (N+1) for _ in range(N+1)]
+render_prefix()
+
+# 각 좌표에 대해 계산 후 출력
 for target in targets:
-    # 각 좌표정보에 대해 계산 후 출력
     print(get_area(target))
