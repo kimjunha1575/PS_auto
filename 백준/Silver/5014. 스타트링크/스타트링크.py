ece29f5c1ef802b5a01eaec3cf340d14284dc6cd
Junha Kim <kimjunha1575@gmail.com>
2024-08-02 19:17:46 +0900

    [Silver I] Title: 스타트링크, Time: 332 ms, Memory: 202200 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,24 +1,36 @@
 from collections import deque
+
+
 def solve():
     que = deque()
-    que.append((S, 0))
     visited = set()
+    
+    # 시작위치 설정
+    que.append((S, 0))
     visited.add(S)
+    
+    # 순회 시작
     while que:
         cur, cnt = que.popleft()
+        # 도착했다면 이동 회수를 반환
         if cur == G:
             return cnt
+        # 각 방향에 대해 방문한 적이 없고, 이동할 수 있는 범위라면 que에 추가
         if cur + U <= F and cur + U not in visited:
             que.append((cur + U, cnt + 1))
             visited.add(cur + U)
         if cur - D >= 1 and cur - D not in visited:
             que.append((cur - D, cnt + 1))
             visited.add(cur - D)
+    # 목적지에 도착하지 못했다면 -1 반환
     return -1
 
 
+# 입력
 F, S, G, U, D = map(int, input().split())
+# 순회
 ans = solve()
+# 도착했다면 이동 회수를, 도착할 수 없다면 use the stairs 출력
 if ans != -1:
     print(ans)
 else:
