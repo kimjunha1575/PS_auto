8e6a7fb5bd71879b2a4486ad117c2ce5af2d7a00
Junha Kim <kimjunha1575@gmail.com>
2024-08-02 11:40:51 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 228 ms, Memory: 132832 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+
+N, K = map(int, input().split())
+que = deque()
+que.append((N, 0))
+visited = dict()
+visited[N] = 0
+ans = []
+fastest_time = 100_001
+move_cnt = 0
+while que:
+    cur_position, cur_time = que.popleft()
+    if cur_time > fastest_time: break
+    if cur_position == K:
+        ans.append(cur_time)
+        fastest_time = cur_time
+    targets = [cur_position - 1, cur_position + 1, cur_position * 2]
+    for target in targets:
+        if target < 0: continue
+        if target > max(N, K) * 2 + 1: continue
+        if visited.get(target) is not None and cur_time + 1 > visited[target]: continue
+        if visited.get(target) is None:
+            visited[target] = cur_time+1
+        que.append((target, cur_time + 1))
+print(fastest_time)
+print(ans.count(fastest_time))
+
