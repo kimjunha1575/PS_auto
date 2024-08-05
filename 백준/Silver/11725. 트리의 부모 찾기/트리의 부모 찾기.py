7fc271f0a2966a15dcb010da73d4bc7eb7256fc2
Junha Kim <kimjunha1575@gmail.com>
2024-08-05 16:11:05 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 240 ms, Memory: 127860 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,42 +1,17 @@
-def find_parent_recursion(idx):
-    global visited
-    global parents
-    visited[idx] = 1
-    children = tree[idx]
-    for child in children:
-        if visited[child]:
-            continue
-        visited[child] = 1
-        parents[child] = idx
-        find_parent_recursion(child)
 
-
-def find_parent_loop(idx, root):
-    global visited
-    global parents
-    que = [root]
-    while que:
-        cur_node = que.pop(0)
-        visited[cur_node] = 1
-        targets = tree[cur_node]
-        for target in targets:
-            if visited[target]:
-                continue
-            parents[target] = cur_node
-            que.append(target)
-    return None
-
-
-tree_size = int(input())
-tree = [[] for _ in range(tree_size + 1)]
-for _ in range(tree_size - 1):
-    m, n = map(int, input().split())
-    tree[m].append(n)
-    tree[n].append(m)
-
-parents = [None] * (tree_size + 1)
-visited = [0] * (tree_size + 1)
-find_parent_loop(1, 1)
-
-for node in range(2, tree_size + 1):
-    print(parents[node])
+V = int(input())
+tree = [[] for _ in range(V+1)]
+for _ in range(V-1):
+    st, en = map(int, input().split())
+    tree[st].append(en)
+    tree[en].append(st)
+parent = [0] * (V+1)
+parent[1] = 1
+stk = [1]
+while stk:
+    cur = stk.pop()
+    for vtx in tree[cur]:
+        if parent[vtx]: continue
+        parent[vtx] = cur
+        stk.append(vtx)
+print(*parent[2:], sep='\n')
