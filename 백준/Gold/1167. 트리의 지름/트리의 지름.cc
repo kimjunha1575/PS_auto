073718f2814c83d21873fb66336d6bff23ab1a1d
Junha Kim <kimjunha1575@gmail.com>
2024-08-05 10:35:33 +0900

    [Gold II] Title: 트리의 지름, Time: 228 ms, Memory: 14868 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -24,13 +25,11 @@ void init() {
 }
 
 int search(int vertex, int cur_acc, int prev_weight) {
-    // printf("visited %d\n", vertex);
     edges[vertex].push_back(cur_acc);
     bool is_edge = true;
     for (int i = 0; i < tree[vertex].size(); i++) {
-        pair<int, int> next = tree[vertex][i];
-        int next_vertex = next.first;
-        int next_weight = next.second;
+        int next_vertex = tree[vertex][i].first;
+        int next_weight = tree[vertex][i].second;
         if (visited[next_vertex]) continue;
         is_edge = false;
         visited[next_vertex] = 1;
@@ -42,14 +41,8 @@ int search(int vertex, int cur_acc, int prev_weight) {
         return prev_weight;
     }
     else {
-        // return max value of edges[vertex]
-        int max = 0;
-        for (int i = 1; i < edges[vertex].size(); i++) {
-            int tmp = edges[vertex][i];
-            if (tmp > max) max = tmp;
-        }
-        // printf("vertex: %d, max:%d, prev_weight:%d\n", vertex, max, prev_weight);
-        return max + prev_weight;
+        sort(edges[vertex].begin()+1, edges[vertex].end());
+        return prev_weight + edges[vertex][edges[vertex].size()-1];
     }
 }
 
@@ -58,25 +51,10 @@ int main(void) {
     init();
     visited[1] = 1;
     search(1, 0, 0);
-    int max = 0;
+    int ans = 0;
     for (int i = 1; i < V+1; i++) {
-        int buf1 = 0;
-        int buf2 = 0;
-        // printf("%d:", i);
-        for (int j = 0; j < edges[i].size(); j++) {
-            int tmp = edges[i][j];
-            // printf("%d, ", tmp);
-            if (tmp > buf1) {
-                if (buf1 > buf2) {
-                    buf2 = buf1;
-                }
-                buf1 = tmp;
-            } else if (tmp > buf2) {
-                buf2 = tmp;
-            }
-        }
-        if ((buf1 + buf2) > max) max = buf1 + buf2;
-        // printf("\n");
+        int tmp = edges[i][edges[i].size()-1] + edges[i][edges[i].size()-2];
+        ans = max(ans, tmp);
     }
-    printf("%d\n", max);
+    printf("%d\n", ans);
 }
