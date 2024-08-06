f61b3e8462dd74fcfa14826e9bcff721899875e1
Junha Kim <kimjunha1575@gmail.com>
2024-08-06 17:28:16 +0900

    [Silver III] Title: N과 M (4), Time: 140 ms, Memory: 114052 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,10 +1,15 @@
 def solve(idx, cnt):
+    # M개를 모두 선택했다면 출력 후 재귀 종료
     if cnt == M:
         print(*buf, sep=' ')
         return
+    # idx부터 N까지 자연수에 대해 중복을 허용하여 선택
     for i in range(idx, N + 1):
+        # i를 선택 후
         buf.append(i)
+        # 재귀 호출
         solve(i, cnt + 1)
+        # buf 배열 복구
         buf.pop()
 
 
