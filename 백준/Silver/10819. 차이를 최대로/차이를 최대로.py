dc4d7caf5e900a6717f085bd4b065cf937598ade
Junha Kim <kimjunha1575@gmail.com>
2024-08-07 14:25:04 +0900

    [Silver II] Title: 차이를 최대로, Time: 136 ms, Memory: 112164 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,12 +1,13 @@
 def solve(cnt, acc, prev):
     global cur_best
-    if acc + 200 * (N-cnt) < cur_best:
+    if acc + largest_gap * (N-cnt) < cur_best:
         return
     if cnt == N:
         cur_best = max(cur_best, acc)
         return
     for i in range(N):
-        if used[i]: continue
+        if used[i]:
+            continue
         used[i] = 1
         solve(cnt + 1, acc + abs(arr[i] - prev), arr[i])
         used[i] = 0
@@ -16,6 +17,16 @@ N = int(input())
 arr = list(map(int, input().split()))
 cur_best = 0
 used = [0] * N
+arr.sort()
+largest_gap = arr[N-1] - arr[0]
+arranged = [0] * N
+arranged[0] = arr[N//2 - 1]
+arranged[N-1] = arr[N//2]
+for i in range(0, N//2-1):
+    arranged[2*(i+1)] = arr[i]
+for i in range(N-1, N//2, -1):
+    arranged[2*(N-i-1)+1] = arr[i]
+arr = arranged
 for i in range(N):
     used[i] = 1
     solve(1, 0, arr[i])
