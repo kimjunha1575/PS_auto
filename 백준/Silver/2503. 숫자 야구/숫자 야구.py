3667bb67353252993856f5c0cd192c5f27ee3d4b
Junha Kim <kimjunha1575@gmail.com>
2024-08-12 19:58:46 +0900

    [Silver III] Title: 숫자 야구, Time: 100 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,31 +1,46 @@
+'''
+그냥 123부터 987까지의 모든 순열에 대해 검사함
+각 자리가 같으면 스트라이크에서 1을 빼고
+다르지만 사용된 숫자라면 볼에서 1을 빼서
+스트라이크와 볼이 둘 다 0이라면 정답에 1을 더함
+'''
+
+
 def is_possible(arr):
     for query in queries:
+        # 모든 문답에 대해
         num = list(map(int, str(query[0])))
         strike = query[1]
         ball = query[2]
-        for digit in range(3):
-            if num[digit] == arr[digit]:
+        for x, y in zip(num, arr):
+            # 일치한다면 스트라이크 - 1
+            if y == x:
                 strike -= 1
-            elif num[digit] in arr:
+            # 자리는 다르지만 사용한 숫자라면 볼 -1
+            elif y in num:
                 ball -= 1
+        # 하나라도 0이 아니라면 즉시 종료
         if strike or ball:
             return False
+    # 모든 문답에 대해 결과가 일치한다면 True 반환
     return True
 
 
 def solve(idx):
     if idx == 3:
         if is_possible(buf):
+            # 모든 문답에 대해 결과가 일치한다면 정답 + 1
             global ans
             ans += 1
         return
     for i in range(1, 10):
+        # 1 ~ 9까지 중복 없이 세 자리를 뽑는 순열
         if used[i]: continue
-        buf.append(i)
         used[i] = 1
+        buf.append(i)
         solve(idx + 1)
-        used[i] = 0
         buf.pop()
+        used[i] = 0
 
 
 N = int(input())
