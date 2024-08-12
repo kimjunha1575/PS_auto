ee05ac9b6bbc753007eaa30c4d6a5bed4da91078
Junha Kim <kimjunha1575@gmail.com>
2024-08-12 14:38:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 116 ms, Memory: 110668 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,29 +1,30 @@
-def my_sort(arr, left, right):
-    if right < left:
-        return
-    pivot = right
-    right -= 1
-    while left <= right:
-        if arr[right] >= arr[pivot]:
-            right -= 1
-            continue
-        if arr[left] <= arr[pivot]:
-            left += 1
-            continue
-        if arr[right] < arr[pivot] < arr[left]:
-            arr[right], arr[left] = arr[left], arr[right]
-            left += 1
-            right -= 1
-    arr[pivot], arr[left] = arr[left], arr[pivot]
-    my_sort(arr, 0, right)
-    my_sort(arr, left + 1, pivot)
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    left_idx = 0
+    right_idx = 0
+    res = []
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            res.append(left[left_idx])
+            left_idx += 1
+        else:
+            res.append(right[right_idx])
+            right_idx += 1
+    res.extend(left[left_idx:])
+    res.extend(right[right_idx:])
+    return res
 
 
 N = int(input())
-li = []
-for _ in range(N):
-    li.append(int(input()))
-
-my_sort(li, 0, len(li) - 1)
-for e in li:
+ori = [int(input()) for _ in range(N)]
+ans = merge_sort(ori)
+for e in ans:
     print(e)
