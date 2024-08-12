23714e65c16f6f8f722c0c12265fe4bc54e6ce92
Junha Kim <kimjunha1575@gmail.com>
2024-08-12 19:43:44 +0900

    [Silver III] Title: 숫자 야구, Time: 104 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -3,19 +3,17 @@ def is_possible(arr):
         num = list(map(int, str(query[0])))
         strike = query[1]
         ball = query[2]
-        strike_cnt = 0
-        ball_cnt = 0
         for digit in range(3):
             if num[digit] == arr[digit]:
-                strike_cnt += 1
+                strike -= 1
             elif num[digit] in arr:
-                ball_cnt += 1
-        if strike != strike_cnt or ball != ball_cnt:
+                ball -= 1
+        if strike or ball:
             return False
     return True
 
 
-def choose_digit(idx):
+def solve(idx):
     if idx == 3:
         if is_possible(buf):
             global ans
@@ -25,15 +23,15 @@ def choose_digit(idx):
         if used[i]: continue
         buf.append(i)
         used[i] = 1
-        choose_digit(idx + 1)
+        solve(idx + 1)
         used[i] = 0
         buf.pop()
 
 
 N = int(input())
-ans = 0
+queries = [tuple(map(int, input().split())) for _ in range(N)]
 buf = []
 used = [0] * 10
-queries = [tuple(map(int, input().split())) for _ in range(N)]
-choose_digit(0)
+ans = 0
+solve(0)
 print(ans)
