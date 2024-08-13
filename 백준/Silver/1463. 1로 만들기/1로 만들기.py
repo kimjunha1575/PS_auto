947504134e0a7ad7676647745f354519aff8c0bd
Junha Kim <kimjunha1575@gmail.com>
2024-08-13 11:17:35 +0900

    [Silver III] Title: 1로 만들기, Time: 260 ms, Memory: 152004 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -2,20 +2,22 @@ from collections import deque
 
 
 N = int(input())
-dp = [0, 1] + [0] * N
 que = deque()
+visited = [0] * (N + 1)
 que.append((1, 1))
+visited[1] = 1
+ans = 0
 while que:
-    if dp[N]: break
     value, steps = que.popleft()
-    if value + 1 <= N and not dp[value + 1]:
-        dp[value + 1] = steps + 1
-        que.append((value + 1, steps + 1))
-    if value * 2 <= N and not dp[value * 2]:
-        dp[value * 2] = steps + 1
-        que.append((value * 2, steps + 1))
-    if value * 3 <= N and not dp[value * 3]:
-        dp[value * 3] = steps + 1
-        que.append((value * 3, steps + 1))
+    if value == N:
+        ans = steps - 1
+        break
+    targets = [value + 1, value * 2, value * 3]
+    for target in targets:
+        if target > N: continue
+        if visited[target]: continue
+        que.append((target, steps + 1))
+        visited[target] = 1
 
-print(dp[N] - 1)
+
+print(ans)
