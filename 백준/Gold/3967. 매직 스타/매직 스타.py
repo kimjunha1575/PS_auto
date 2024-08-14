63927dba59ce44e245f3bc901135642d38ec9eda
Junha Kim <kimjunha1575@gmail.com>
2024-08-14 19:21:06 +0900

    [Gold V] Title: 매직 스타, Time: 164 ms, Memory: 113164 KB -BaekjoonHub

--- 백준/Gold/3967. 매직 스타/매직 스타.py
+++ 백준/Gold/3967. 매직 스타/매직 스타.py
@@ -1,20 +1,10 @@
 '''
-1 2 3 4
-0 2 5 7
-0 3 6 10
-7 8 9 10
-1 5 8 11
-4 6 9 11
-
-그냥 완전 주먹구구식으로 한번 해보고싶어서 해봤습니다
-자리를 채우는 '.'이 아닌 문자만 순서대로 받아서 따로 배열에 순서대로 저장한 다음
-각 인덱스 별로 '매직스타'에서 한 줄을 이루는 인덱스들을 미리 정리해두고 (주석 맨 위)
-그냥 모든 경우에 대해 순서대로 빈 자리에 순열을 넣어서
-유효한 매직스타인지 검사 후 맞으면 출력하고 종료했습니다
+매직 스타를 이루는 부분들만 따로 리스트에 저장
+매직 스타를 이루는 각 인덱스들에 대해 직접 접근해서 유효성 검사
 '''
 
 
-def prt_board():
+def print_star():
     print(f"....{chr(board_list[0] + 64)}....")
     print(f".{chr(board_list[1] + 64)}.{chr(board_list[2] + 64)}.{chr(board_list[3] + 64)}.{chr(board_list[4] + 64)}.")
     print(f"..{chr(board_list[5] + 64)}...{chr(board_list[6] + 64)}..")
@@ -26,58 +16,55 @@ def get_sum(one, two, three, four):
     return board_list[one] + board_list[two] + board_list[three] + board_list[four]
 
 
-def verify():
-    if get_sum(1, 2, 3, 4) > 26:
-        return False
-    if get_sum(0, 2, 5, 7) > 26:
-        return False
-    if get_sum(0, 3, 6, 10) > 26:
-        return False
-    if get_sum(7, 8, 9, 10) > 26:
-        return False
-    if get_sum(1, 5, 8, 11) > 26:
-        return False
-    if get_sum(4, 6, 9, 11) > 26:
-        return False
+def is_occupied(one, two, three, four):
+    return board_list[one] and board_list[two] and board_list[three] and board_list[four]
+
+def is_possible():
+    if get_sum(1, 2, 3, 4) > 26: return False
+    if is_occupied(1, 2, 3, 4) and get_sum(1, 2, 3, 4) != 26: return False
+    if get_sum(0, 2, 5, 7) > 26: return False
+    if is_occupied(0, 2, 5, 7) and get_sum(0, 2, 5, 7) != 26: return False
+    if get_sum(0, 3, 6, 10) > 26: return False
+    if is_occupied(0, 3, 6, 10) and get_sum(0, 3, 6, 10) != 26: return False
+    if get_sum(7, 8, 9, 10) > 26: return False
+    if is_occupied(7, 8, 9, 10) and get_sum(7, 8, 9, 10) != 26: return False
+    if get_sum(1, 5, 8, 11) > 26: return False
+    if is_occupied(1, 5, 8, 11) and get_sum(1, 5, 8, 11) != 26: return False
+    if get_sum(4, 6, 9, 11) > 26: return False
+    if is_occupied(4, 6, 9, 11) and get_sum(4, 6, 9, 11) != 26: return False
     return True
 
 
-def is_possible():
-    if get_sum(1, 2, 3, 4) != 26:
-        return False
-    if get_sum(0, 2, 5, 7) != 26:
-        return False
-    if get_sum(0, 3, 6, 10) != 26:
-        return False
-    if get_sum(7, 8, 9, 10) != 26:
-        return False
-    if get_sum(1, 5, 8, 11) != 26:
-        return False
-    if get_sum(4, 6, 9, 11) != 26:
-        return False
+def is_valid():
+    if get_sum(1, 2, 3, 4) != 26: return False
+    if get_sum(0, 2, 5, 7) != 26: return False
+    if get_sum(0, 3, 6, 10) != 26: return False
+    if get_sum(7, 8, 9, 10) != 26: return False
+    if get_sum(1, 5, 8, 11) != 26: return False
+    if get_sum(4, 6, 9, 11) != 26: return False
     return True
 
 
 def solve(n):
     global done
     if done: return
-    if not verify():
-        return
+    if not is_possible(): return
     if n == 12:
-        if is_possible():
+        if is_valid():
             done = True
-            prt_board()
+            print_star()
             return
     for i in range(1, 13):
         if used[i]: continue
         used[i] = 1
+        cur_idx = 0
         for j in range(12):
             if board_list[j] == 0:
-                tmp = j
+                cur_idx = j
                 board_list[j] = i
                 break
         solve(n+1)
-        board_list[tmp] = 0
+        board_list[cur_idx] = 0
         used[i] = 0
 
 
