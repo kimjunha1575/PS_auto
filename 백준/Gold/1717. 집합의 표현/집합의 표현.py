4243708a069ed6da2823194f318b41dc4778b1be
Junha Kim <kimjunha1575@gmail.com>
2024-08-14 16:16:52 +0900

    [Gold V] Title: 집합의 표현, Time: 244 ms, Memory: 279400 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -4,17 +4,20 @@ union-find (disjoint set)
 '''
 import sys
 sys.setrecursionlimit(100000)
+input = sys.stdin.readline
 
 
-def find(p):
+def find(p, d):
+    global depth
+    depth = max(depth, d)
     if parent[p] != p:
-        parent[p] = find(parent[p])
+        parent[p] = find(parent[p], d + 1)
     return parent[p]
 
 
 def union(p, q):
-    pp = find(p)
-    pq = find(q)
+    pp = find(p, 0)
+    pq = find(q, 0)
     if pp == pq: return
     if pp < pq:
         parent[pq] = pp
@@ -24,12 +27,16 @@ def union(p, q):
 
 n, m = map(int, input().split())
 parent = [i for i in range(n+1)]
+depth = 0
 for _ in range(m):
     command, p, q = map(int, input().split())
     if command:
-        if find(p) == find(q):
+        if find(p, 0) == find(q, 0):
             print("YES")
         else:
             print("NO")
     else:
         union(p, q)
+
+# print(parent)
+# print(depth)
