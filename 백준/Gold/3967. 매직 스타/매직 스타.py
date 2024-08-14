3c8890e0336af730267eb740fd6caf2ecee2e2c7
Junha Kim <kimjunha1575@gmail.com>
2024-08-14 18:48:35 +0900

    [Gold V] Title: 매직 스타, Time: 172 ms, Memory: 112024 KB -BaekjoonHub

--- 백준/Gold/3967. 매직 스타/매직 스타.py
+++ 백준/Gold/3967. 매직 스타/매직 스타.py
@@ -5,6 +5,12 @@
 7 8 9 10
 1 5 8 11
 4 6 9 11
+
+그냥 완전 주먹구구식으로 한번 해보고싶어서 해봤습니다
+자리를 채우는 '.'이 아닌 문자만 순서대로 받아서 따로 배열에 순서대로 저장한 다음
+각 인덱스 별로 '매직스타'에서 한 줄을 이루는 인덱스들을 미리 정리해두고 (주석 맨 위)
+그냥 모든 경우에 대해 순서대로 빈 자리에 순열을 넣어서
+유효한 매직스타인지 검사 후 맞으면 출력하고 종료했습니다
 '''
 
 
@@ -18,6 +24,22 @@ def prt_board():
 
 def get_sum(one, two, three, four):
     return board_list[one] + board_list[two] + board_list[three] + board_list[four]
+
+
+def verify():
+    if get_sum(1, 2, 3, 4) > 26:
+        return False
+    if get_sum(0, 2, 5, 7) > 26:
+        return False
+    if get_sum(0, 3, 6, 10) > 26:
+        return False
+    if get_sum(7, 8, 9, 10) > 26:
+        return False
+    if get_sum(1, 5, 8, 11) > 26:
+        return False
+    if get_sum(4, 6, 9, 11) > 26:
+        return False
+    return True
 
 
 def is_possible():
@@ -39,6 +61,8 @@ def is_possible():
 def solve(n):
     global done
     if done: return
+    if not verify():
+        return
     if n == 12:
         if is_possible():
             done = True
