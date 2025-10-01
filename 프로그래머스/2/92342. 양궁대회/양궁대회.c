90375a96f1d41c9900eec292b700417f0c7e74ae
Junha Kim <kimjunha1575@gmail.com>
2025-10-02 01:14:06 +0900

    [level 2] Title: 양궁대회, Time: 6.87 ms, Memory: 9.06 MB -BaekjoonHub

---
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// info_len은 배열 info의 길이입니다.
+int* solution(int n, int info[], size_t info_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(1);
+    return answer;
+}
